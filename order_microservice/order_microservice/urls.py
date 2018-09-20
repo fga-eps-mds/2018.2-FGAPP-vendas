5797c8d0c32181a55f9aaa95c6732f9a0faed79b
Lucas Costa <lucascostaa73@gmail.com>
2018-09-20 20:52:55 -0300

    Deleting old files project, and creating new base project

--- order_microservice/order_microservice/urls.py
+++ order_microservice/order_microservice/urls.py
@@ -1,4 +1,4 @@
-"""selling URL Configuration
+"""order_microservice URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
