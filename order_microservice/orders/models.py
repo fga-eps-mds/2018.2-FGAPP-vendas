791a5359f2ab88312bd4b0bc7cae5243646dc575
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-09-23 22:37:54 -0300

    Configuring function: 'create_order' responses
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>

--- order_microservice/orders/models.py
+++ order_microservice/orders/models.py
@@ -4,7 +4,7 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add= True)
     fk_product = models.IntegerField(default= '0')
     fk_buyer = models.IntegerField(default= '0')
-    buyer_message = models.TextField()
+    buyer_message = models.CharField(max_length=140)
     quantity = models.IntegerField(default= '0')
     total_price = models.FloatField(default= '0.0')
     closed = models.BooleanField(default= False)
--- order_microservice/orders/views.py
+++ order_microservice/orders/views.py
@@ -4,6 +4,13 @@ from rest_framework.decorators import api_view
 from rest_framework import generics
 from .models import Order
 from .serializers import OrderSerializer
+from rest_framework.status import (
+    HTTP_403_FORBIDDEN,
+    HTTP_200_OK,
+    HTTP_404_NOT_FOUND,
+    HTTP_400_BAD_REQUEST,
+)
+from rest_framework.response import Response
 
 @api_view(["POST"])
 def create_order(request):
@@ -11,7 +18,19 @@ def create_order(request):
     fk_buyer = request.data.get('fk_buyer')
     buyer_message = request.data.get('buyer_message')
     quantity = request.data.get('quantity')
-    product_price = request.data.get('product_price')
+    total_price = request.data.get('total_price')
+
+    try:
+        product = Order.objects.create(
+            fk_buyer = fk_buyer,
+            fk_product = fk_product,
+            buyer_message = buyer_message,
+            quantity = quantity,
+            total_price = total_price)
+        return Response(status=HTTP_200_OK)
+    except:
+        return Response(status=HTTP_400_BAD_REQUEST)
+
 
 class OrderList(generics.ListCreateAPIView):
     queryset = Order.objects.all()
