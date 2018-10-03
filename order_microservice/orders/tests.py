96dd9f90b23ee3f2724d3c1031243152309e5f6c
Andre Lucas <andrelucax@gmail.com>
2018-10-03 01:55:18 -0300

    Fixed test bug
    Co-authored-by: Andre Lucas de Sousa Pinto <andrelucax@gmail.com>
    Co-authored-by: Damaso Junio Pereira Brasileo <junio.pereirab@gmail.com>

--- order_microservice/orders/tests.py
+++ order_microservice/orders/tests.py
@@ -7,17 +7,17 @@ class CheckOrderAPITest(APITestCase):
     def test_create_order(self):
         # OK if can create
         request_1 = {'fk_product':'1', 'fk_buyer':'1', 'buyer_message':'Estou no MASP do lado da entrada do RU.', 'quantity':'1', 'total_price':'1.23', 'product_name': 'Bolo no Pote'}
-        response_1 = self.client.post('/api/create_order', request_1)
+        response_1 = self.client.post('/api/create_order/', request_1)
         self.assertEqual(response_1.status_code, 200)
 
         # BAD REQUEST if request has not any field
         request_2 = {'fk_product':'1', 'buyer_message':'Estou no MASP do lado da entrada do RU.', 'quantity':'1', 'total_price':'1.23', 'product_name': 'Bolo no Pote'}
-        response_2 = self.client.post('/api/create_order', request_2)
+        response_2 = self.client.post('/api/create_order/', request_2)
         self.assertEqual(response_2.status_code, 400)
 
         # BAD REQUEST if some request field does not match
         request_3 = {'fk_product':'Errei', 'fk_buyer':'1', 'buyer_message':'Estou no MASP do lado da entrada do RU.', 'quantity':'1', 'total_price':'1.23', 'product_name': 'Bolo no Pote'}
-        response_3 = self.client.post('/api/create_order', request_3)
+        response_3 = self.client.post('/api/create_order/', request_3)
         self.assertEqual(response_3.status_code, 400)
 
     def test_user_orders(self):
