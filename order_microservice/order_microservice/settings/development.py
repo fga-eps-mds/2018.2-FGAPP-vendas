28d164b9d25b96baae808f6581e807db0ce24cae
Lucas Costa <lucascostaa73@gmail.com>
2018-10-12 04:47:36 -0300

    Fixing settings

--- order_microservice/order_microservice/settings/development.py
+++ order_microservice/order_microservice/settings/development.py
@@ -1,26 +1,13 @@
 from order_microservice.settings.common import *
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&#%@946mnozni(ta@4d1m^d2b3o9j#2v*h9)br(_yqm2h(6=-4'
+SECRET_KEY = 't@8!=auy=1zf#_p0c-kttunhohb!rea=a3ckkhdupb#hei)dyu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
